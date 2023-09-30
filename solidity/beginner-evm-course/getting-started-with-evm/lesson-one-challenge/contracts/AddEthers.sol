8893aa151d720144de64810a7ef4facfb6478afc
olorunfemibabs <olorunfemisamuel01@gmail.com>
2023-09-30 15:27:18 +0100

    addethers

---
+++ solidity/beginner-evm-course/getting-started-with-evm/lesson-one-challenge/contracts/AddEthers.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract AddEthers {
+
+    mapping (address => uint) public balance;
+
+    function deposit () external payable {
+        balance[msg.sender] += msg.value;
+    }
+
+    function valuewei () public view returns(uint) {
+        return balance[msg.sender];
+    }
+
+    function valueether () public view returns (uint) {
+        return balance[msg.sender]/1 ether;
+    }
+
+    function valuegwei () public view returns (uint) {
+        return balance[msg.sender]/1 gwei;
+    }
+
+}
